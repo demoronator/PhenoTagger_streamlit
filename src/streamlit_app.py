e48d1b8144eb400dcded21c8fc4aa42ca7c36533
Wonyoung Chung <demoronator@gmail.com>
2024-03-03 13:40:58 -0500

    Fix Streamlit deployment

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -6,18 +6,18 @@ from nn_model import bioTag_BERT
 @st.cache_resource
 def load_model():
     ontfiles={
-        'dic_file':'../dict/noabb_lemma.dic',
-        'word_hpo_file':'../dict/word_id_map.json',
-        'hpo_word_file':'../dict/id_word_map.json'
+        'dic_file':'dict/noabb_lemma.dic',
+        'word_hpo_file':'dict/word_id_map.json',
+        'hpo_word_file':'dict/id_word_map.json'
     }
     biotag_dic = dic_ont(ontfiles)
 
     vocabfiles={
-        'labelfile':'../dict/lable.vocab',
-        'checkpoint_path':'../models/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/',
+        'labelfile':'dict/lable.vocab',
+        'checkpoint_path':'models/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext/',
         'lowercase':True
     }
-    modelfile='../models/pubmedbert_PT_v1.2.h5'
+    modelfile='models/pubmedbert_PT_v1.2.h5'
     nn_model=bioTag_BERT(vocabfiles)
     nn_model.load_model(modelfile)
 
